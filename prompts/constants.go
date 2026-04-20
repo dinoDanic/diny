16f26dc84a73b7865dfae11e994d446020cdc560
dinoDanic <dino.danic@gmail.com>
2026-04-20 08:46:36 +0200

    always trigger prompt on eligible commits

--- prompts/constants.go
+++ prompts/constants.go
@@ -1,11 +1,8 @@
 package prompts
 
 const (
-	// MinCommitsBeforePrompt is the number of successful commits before any prompt can appear.
-	MinCommitsBeforePrompt = 5
-
 	// TriggerProbability is the chance a prompt appears on an eligible commit.
-	TriggerProbability = 0.15
+	TriggerProbability = 1.0
 
 	// GitHubRepoURL is opened in the browser when the user chooses to star.
 	GitHubRepoURL = "https://github.com/dinoDanic/diny"
